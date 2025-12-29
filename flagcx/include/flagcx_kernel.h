49bc1b42d3f7ae63773cc6d990af844d1f7a97b1
liuhe <liuhe@infrawaves.com>
2025-12-29 16:14:24 +0800

    add test_kernel_put

--- flagcx/include/flagcx_kernel.h
+++ flagcx/include/flagcx_kernel.h
@@ -128,5 +128,11 @@ flagcxDeviceSignal(size_t dstOffset, int peer, void *fifoBuffer);
 void flagcxP2pDemo(const void *sendbuff, void *recvbuff, size_t count,
                    flagcxDataType_t datatype, int sendPeer, int recvPeer,
                    flagcxComm_t comm, flagcxStream_t stream);
+void flagcxOnesidedSendDemo(const void *srcbuff, size_t srcOffset,
+                            size_t dstOffset, size_t signalOffset, size_t count,
+                            flagcxDataType_t datatype, int peer,
+                            flagcxComm_t comm, flagcxStream_t stream);
+void flagcxOnesidedRecvDemo(volatile uint64_t *waitAddr, uint64_t expectedValue,
+                            flagcxComm_t comm, flagcxStream_t stream);
 #endif // COMPILE_KERNEL
 #endif
